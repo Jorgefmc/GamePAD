eea840645c55c4357d6699d6fcbc6a8e76e8ede0
Ynscription <ynscription@gmail.com>
2018-06-21 13:42:56 +0200

    Removed unused code from OffersFragment.

--- app/src/main/java/gamepad/pad/OffersFragment.java
+++ app/src/main/java/gamepad/pad/OffersFragment.java
@@ -158,28 +158,5 @@ class OffersItemListAdapter extends  RecyclerView.Adapter<OffersItemListAdapter.
         return _gameList.length;
     }
 
-
-
-    private class OffersLocationListener implements LocationListener {
-
-        @Override
-        public void onLocationChanged(Location location) {
-
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    }
+    
 }
